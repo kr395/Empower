860038a54153a4c6b585f1fc318283363c060a79
Gopal Kumar Das <krgopalkumar706@gmail.com>
2023-08-02 17:39:31 +0530

    live time added in login page

--- login/login.js
+++ login/login.js
@@ -5,3 +5,12 @@ const dayDateView = document.querySelector("#dayDate")
 const timeView = document.querySelector("#time")
 dayDateView.innerHTML =  `${today.getDay()} ${today.getMonth()} ${today.getFullYear()}`
 
+
+setInterval(() => {
+const time = new Date()    
+const hrs = time.getHours()
+const min = time.getMinutes()
+const sec = time.getSeconds()
+timeView.innerHTML = `${hrs} : ${min} : ${sec} `
+}, 1000);
+
