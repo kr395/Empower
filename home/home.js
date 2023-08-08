2252dbc398edff256670ffe292a625f097b7641e
Gopal Kumar Das <krgopalkumar706@gmail.com>
2023-08-08 17:21:49 +0530

    Home page icon added

--- home/home.js
+++ home/home.js
@@ -2,7 +2,23 @@
 //  Login Validation
 const body = document.querySelector("body")
 const helloSpan = document.querySelector(".welcome")
+const user = JSON.parse(localStorage.getItem("logedUser"))
 body.onload = ()=>{
-      const user = JSON.parse(localStorage.getItem("logedUser"))
+    if (!user) {
+        window.alert("Please Login again")
+        window.location.href = "http://127.0.0.1:5500/login/login.html"
+    } else {
       helloSpan.innerHTML = `Hello ${user.userName}`
     }
+    }
+// Logoout Functunality
+
+// const logoutbtn = document.querySelector("#logout_btn")
+// logoutbtn.addEventListener('click',()=>{
+//     localStorage.removeItem("logedUser")
+//     window.location.href = "http://127.0.0.1:5500/login/login.html"
+// })
+
+// Dropdown Animation
+const icons = document.querySelectorAll("i")
+console.log(icons)
