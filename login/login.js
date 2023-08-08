ff9ed478f33153b28ecf5ca7b5cb1e564084d3f2
Gopal Kumar Das <krgopalkumar706@gmail.com>
2023-08-08 12:06:04 +0530

    Login.js complited with old code

--- login/login.js
+++ login/login.js
@@ -1,19 +1,8 @@
 //  Date Of Today View
 
-const today = new Date();
 const dayDateView = document.querySelector("#dayDate");
 const timeView = document.querySelector("#time");
-const weekday = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = weekday[today.getDay()];
-let month;
+const today = new Date();
 let timeText = String(today);
 let timeTextSlice = timeText.slice(0, 15);
 dayDateView.innerHTML = timeTextSlice;
@@ -21,7 +10,7 @@ dayDateView.innerHTML = timeTextSlice;
 //  Live Changing Time
 setInterval(() => {
   const time = new Date();
-  let hr;
+  let hr = time.getHours();
   let amPm;
   if (time.getHours() == 12) {
     amPm = "PM";
@@ -29,6 +18,8 @@ setInterval(() => {
     hr = time.getHours() - 12;
     amPm = "PM";
   } else if (time.getHours() == 0) {
+    amPm = "AM";
+  } else if (time.getHours() < 12) {
     amPm = "AM";
   }
   const hrs = (hr < 10 ? "0" : " ") + String(hr);
@@ -41,73 +32,79 @@ setInterval(() => {
 
 const inputUserId = document.querySelector("#userId");
 const inputPassword = document.querySelector("#pass");
-const login = document.querySelector("#login");
+const loginBtn = document.querySelector("#login");
 
-// Creating User Data in Local Storage
+// const users = [
+//   { userName: "Mohit", id: "VBS3215", password: "54321", isAdmin: true },
+//   { userName: "Aman", id: "VBS3212", password: "54321", isAdmin: false },
+//   { userName: "Gopal", id: "VBS3211", password: "54321", isAdmin: false },
+//   { userName: "Pranjul", id: "VBS3214", password: "54321", isAdmin: false },
+// ];
+// localStorage.setItem("users", JSON.stringify(users));
 
-// class userCreater {
-//   constructor(userId, password) {
-//     this.userId = userId;
-//     this.password = password;
-//   }
-//   store() {
-//     localStorage.setItem(this.userId, this.password);
-//   }
-// }
-
-// users.every((currentValue,index,arr)=>{
-//     console.log(arr)
-// })
-const checkArray = ["VBS3211"];
-
-const users =[
-    { id: "VBS3215", pass: "54321", admin: true, price: 100 },
-    { id: "VBS3212", pass: "54321", admin: false, price: 100 },
-    { id: "VBS3211", pass: "54321", admin: false, price: 100 },
-    { id: "VBS3214", pass: "54321", admin: false, price: 100 },
-  ];
-
-// const result1 = checkArray.map((val) => {
-//   return users.findIndex((el) => {
-//     return el.id === val;
-//   });
-// });
-
-// console.log(JSON.stringify(result1));
-localStorage.setItem("users", JSON.stringify(users));
-const storage_users = JSON.parse(localStorage.getItem("users"));
-const input = [];
-let id = storage_users.slice(0).every((storage_user, index) => {
-  return (storage_user.id = "VBS3211");
-});
-console.log(storage_users[2].id)
-
-input.push("VBS3213");
-console.log(input);
-
-const result = input.map((key) => {
-  return storage_users.findIndex((b) => {
-    return b.id === key;
+loginBtn.addEventListener("click", () => {
+  const inputIdValue = String(inputUserId.value).toUpperCase();
+  const inputPasswordValue = String(inputPassword.value).toUpperCase();
+  users = JSON.parse(localStorage.getItem("users"));
+  const user = users.find((user) => {
+    return user.id === inputIdValue;
   });
-});
-console.log(result);
-
-login.addEventListener("click", () => {
-  if (inputUserId.value == "" || inputPassword.value == "") {
-    window.alert("Fill all Data");
-  } else {
-    // localStorage.setItem("counter", counter )
-    // const user = new userCreater("Gopal","12345")
-    // user1.store()
-    // count.push(counter)
-    // counter++
-
-    if (id) {
+  function isValidate(id, password) {
+    if (user.id === id && user.password === password) {
       window.location.href = "http://127.0.0.1:5500/home/home.html";
     } else {
-      window.alert("UserId or Password is wrong");
+      window.alert("Invalid Login Details");
     }
+  }
+  if (!user) {
+    window.alert("Fill all Data");
+  } else {
+    isValidate(inputIdValue, inputPasswordValue);
   }
 });
 
+// login.addEventListener("click", () => {
+//   if (inputUserId.value == "" || inputPassword.value == "") {
+//     window.alert("Fill all Data");
+//   } else {
+//     // localStorage.setItem("counter", counter )
+//     // const user = new userCreater("Gopal","12345")
+//     // user1.store()
+//     // count.push(counter)
+//     // counter++
 
+//     if (id) {
+//       window.location.href = "http://127.0.0.1:5500/home/home.html";
+//     } else {
+//       window.alert("Invalid Username or Password");
+//     }
+//   }
+// });
+
+// // Function to initialize user data in localStorage
+// function initializeUsers() {
+//   localStorage.setItem("users", JSON.stringify(users));
+// }
+
+// // Function to check if user credentials are valid
+// function isValidUser(id, password) {
+//   const users = JSON.parse(localStorage.getItem("users"));
+//   if (!users) {
+//     return false;
+//   }
+//   const user = users.find(user => user.id === id && user.password === password);
+//   return !!user;
+// }
+// // Example usage
+// initializeUsers();
+// const usernameInput = "VBS3211"
+// const passwordInput = "54321"
+
+// login.addEventListener('click',(usernameInput,passwordInput)=>{
+//   if (isValidUser(usernameInput, passwordInput)) {
+//     window.location.href = "http://127.0.0.1:5500/home/home.html";
+//   } else {
+//     window.alert("Invalid Username or Password");
+//   }
+
+// })
